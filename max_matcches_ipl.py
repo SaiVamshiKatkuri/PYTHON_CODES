7da406a6794b26a56e0f7accaaea0af1f355ce84
SaiVamshiKatkuri <64994509+SaiVamshiKatkuri@users.noreply.github.com>
2020-05-17 09:52:22 +0530

    Add files via upload

---
+++ max_matcches_ipl.py
@@ -0,0 +1,28 @@
+#Program to find the team which won max matches
+#importing CSV file
+import csv
+with open('matches.csv') as csv_file:
+     matches = csv.reader(csv_file, delimiter=',')
+     d={}
+     l=[]
+     line_c=0
+     for row in matches:
+         if row[10]!='winner':
+           l.append(row[10])
+     dict={s:l.count(s) for s in l}
+     print(dict)
+     key_list=list(dict.keys())
+     val_list=list(dict.values())
+     #finding max matches won by a team
+     a=max(dict.values())
+     print(key_list[val_list.index(a)],end=" ")
+     print(a)
+
+
+
+
+
+
+
+
+
